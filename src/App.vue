543d0c5f3cea8f9753c99ad92778b860524472b0
Clement Ebri <cebri@junipercommerce.com>
2022-04-25 10:55:05 -0400

    font updates

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,3 @@ export default {
 }
 </script>
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
-
-body {
-  background: #f7f7f7;
-  font-family: 'Poppins', sans-serif;
-}
-</style>
